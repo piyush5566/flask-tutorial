282c7a74b4d8dec6e4fa4b5260041dc30a51bfa9
Piyush Kumar <pgh5566@gmail.com>
2024-07-25 18:29:25 +0530

    Added unit tests and fixed spacing in some files

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,39 +1,52 @@
 import sqlite3
 import click
 from flask import current_app, g
+from datetime import datetime
+
+
+# Define custom converter functions
+def adapt_datetime(ts):
+    return ts.strftime("%Y-%m-%d %H:%M:%S")
+
+
+def convert_datetime(ts):
+    return datetime.strptime(ts.decode("utf-8"), "%Y-%m-%d %H:%M:%S")
+
+
+# Register the converter functions with SQLite
+sqlite3.register_adapter(datetime, adapt_datetime)
+sqlite3.register_converter("timestamp", convert_datetime)
 
 
 def get_db():
-    if 'db' not in g:
+    if "db" not in g:
         g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
+            current_app.config["DATABASE"], detect_types=sqlite3.PARSE_DECLTYPES
         )
         g.db.row_factory = sqlite3.Row
-        
+
     return g.db
 
 
 def close_db(e=None):
-    db = g.pop('db', None)
+    db = g.pop("db", None)
     if db is not None:
         db.close()
- 
-        
+
+
 def init_db():
     db = get_db()
-    with current_app.open_resource('schema.sql') as f:
-        db.executescript(f.read().decode('utf-8'))
+    with current_app.open_resource("schema.sql") as f:
+        db.executescript(f.read().decode("utf-8"))
 
 
-@click.command('init-db')
+@click.command("init-db")
 def init_db_command():
     """Clear the existing data and create new tables."""
     init_db()
-    click.echo('Iitialized the database.')
+    click.echo("Initialized the database.")
 
-    
+
 def init_app(app):
     app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
-    
